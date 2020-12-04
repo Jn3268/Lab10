c1ef234b302c2686caaf1c09dcb2c71ce1579417
Jacob Nye <jbnye@umich.edu>
2020-12-03 20:47:19 -0500

    Initial commit

---
+++ testSelectionSort.java
@@ -0,0 +1,27 @@
+package Lab10Package;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class testSelectionSort {
+private SelectionSort temp2;
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+		//fail("Not yet implemented");
+	}
+	 public testSelectionSort() {
+
+	 }
+
+	
+
+
+
+
+
+}
